f9cb4c24042689074c293cfa0f686ecbbe33a0ee
Haymo Meran <haymo.meran@gmail.com>
2019-10-30 07:41:20 +0100

    update

---
+++ build/precache-manifest.71ebde81778d4b55faf5adf9ec3cc5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064337374809de3bf44e1f20431b36b5",
+    "url": "/amplitude/build/index.html"
+  },
+  {
+    "revision": "3d1c6ab2033100589960",
+    "url": "/amplitude/build/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "583f18e2ee6b202d8068",
+    "url": "/amplitude/build/static/css/main.b343fa62.chunk.css"
+  },
+  {
+    "revision": "3d1c6ab2033100589960",
+    "url": "/amplitude/build/static/js/2.c4ea232c.chunk.js"
+  },
+  {
+    "revision": "583f18e2ee6b202d8068",
+    "url": "/amplitude/build/static/js/main.4d44d2e9.chunk.js"
+  },
+  {
+    "revision": "772004251448d7419980",
+    "url": "/amplitude/build/static/js/runtime~main.dc006a2d.js"
+  }
+]);
